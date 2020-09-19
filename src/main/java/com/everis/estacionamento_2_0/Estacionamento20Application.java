00b7ca31e506c9953489b99cdc15c30565884e56
Cleideg Gomides <cleidegomides@hotmail.com>
2020-09-19 18:40:55 -0300

    Iniciando o projeto estacionamento_2_0

---
+++ src/main/java/com/everis/estacionamento_2_0/Estacionamento20Application.java
@@ -0,0 +1,13 @@
+package com.everis.estacionamento_2_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Estacionamento20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Estacionamento20Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/everis/estacionamento_2_0/Estacionamento20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.estacionamento_2_0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Estacionamento20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
